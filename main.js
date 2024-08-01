d34ca29ccaf2b8f26304dd00dd6efcb650bdcab7
antonio9038 <antonio.ramalho@escola.pr.gov.br>
2024-08-01 20:41:38 -0300

    primeiros arquivos
    A postagem dos primeiros arquivos, mas ainda vão acontecer modificações.

---
+++ main.js
@@ -0,0 +1,44 @@
+// Configuração do scroll do cabeçalho;
+
+window.addEventListener("scroll", function () {
+    let cabecalho = document.querySelector('#header');
+    cabecalho.classList.toggle('rolagem', window.scrollY > 590);
+})
+
+// Configuração do carrossel card;
+
+let contador = 1;
+document.getElementById("CardRadio1").checked = true;
+
+setInterval(
+    function () {
+        proximoCartao();
+    }, 5000
+)
+
+function proximoCartao() {
+    contador++;
+    if (contador > 3) {
+        contador = 1;
+    }
+    document.getElementById("CardRadio" + contador).checked = true;
+}
+
+// Confiração do carrossel;
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(
+    function () {
+        proximaImagem();
+    }, 4000
+)
+
+function proximaImagem() {
+    count++;
+    if (count > 4) {
+        count = 1;
+    }
+    document.getElementById("radio" + count).checked = true;
+}
